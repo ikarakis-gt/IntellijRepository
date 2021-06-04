81341dc6ccf67c1049a72777a08cefdbde4a7fa8
Ioannis Karakis <geokarakis@gmail.com>
2021-06-04 16:28:11 +0300

    commit

---
+++ src/com/company/Second_Class.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Second_Class {
+
+    public static void main(String[] args) {
+        int p = 0;
+        int x = 0;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+    }
+
+}
