b8f5defc2230ba9bb5a456fc02d3a866eab9e68d
Ioannis Karakis <geokarakis@gmail.com>
2021-06-04 17:27:53 +0300

    commit

--- src/com/company/First_Class.java
+++ src/com/company/First_Class.java
@@ -5,6 +5,9 @@ public class First_Class {
     public static void main(String[] args) {
         int p = 0;
         int x = 0;
+        int i = 0;
+        int j = 0;
+        int k = 0;
     }
 
 }
