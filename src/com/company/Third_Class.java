8f9657f3e7933bf4deb76fe4e49af68569888817
ikarakis-gt <85347891+ikarakis-gt@users.noreply.github.com>
2021-06-07 11:54:01 +0300

    Update Third_Class.java

--- src/com/company/Third_Class.java
+++ src/com/company/Third_Class.java
@@ -5,5 +5,9 @@ public class Third_Class {
     public void print(String message) {
         System.out.println(message);
     }
+    
+    public void send(String message) {
+        //sending message   
+    }
 
 }
